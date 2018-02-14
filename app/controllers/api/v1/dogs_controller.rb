f3defc0d0f81e5fce3049547586a5a94e50a873d
jamesonbass1987 <jamesonbass@gmail.com>
2018-02-14 09:57:58 -0800

    stubbed out dogs controller

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,5 +1,49 @@
 class Api::V1::DogsController < ApiController
   before_action :set_dog, only: [:show, :update, :destroy]
 
-  
+  def index
+    render json: Dog.all, status: 200
+  end
+
+  def create
+    dog = Dog.new(dog_params)
+    if dog.save
+      render json: dog, status: 200
+    else
+      render json: { message: dog.errors }, status: 400
+    end
+  end
+
+  def show
+    if @dog
+      render json: @dog, status: 200
+    else
+      render json: { message: "Resource not found." }, status: 404
+    end
+  end
+
+  def update
+    if @dog.update(dog_params)
+      render json: @dog, status: 204
+    else
+      render json: { message: @dog.errors }, status: 400
+    end
+  end
+
+  def destroy
+    if @dog.destroy
+      render status: 204
+    else
+      render json: { message: "Unable to remove dog." }, status: 404
+    end
+  end
+
+  private
+    def dog_params
+      params.permit(:name, :user_id, :breed_id, :temperament_id, :age_id, :size_id, :description, :preferred_park_id, :neighborhood_id)
+    end
+
+    def set_dog
+      @dog = Dog.find_by(id: params[:id])
+    end
 end
