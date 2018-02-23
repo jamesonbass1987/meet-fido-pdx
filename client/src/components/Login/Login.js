4d72217aa082769814b4574bb81c6afbe54714b7
jamesonbass1987 <jamesonbass@gmail.com>
2018-02-23 12:41:21 -0800

    added login form validation

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { handleUserLogin } from '../../store/actions/index';
+import { updateObject, checkValidity } from '../../shared/utility';
 
 import { Button, Form, Grid, Header, Image, Segment, Input } from 'semantic-ui-react';
 
@@ -11,30 +12,66 @@ import Icon from '../../assets/images/paw-print.png';
 
 class LoginForm extends Component {
 
+    // state = {
+    //     formData: {
+    //         username: '',
+    //         password: ''
+    //     }
+    // }
+
     state = {
         formData: {
-            username: '',
-            password: ''
+            username: {
+                value: '',
+                validation: {
+                    required: true,
+                },
+                valid: false,
+                touched: false
+            },
+            password: {
+                value: '',
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                valid: false,
+                touched: false
+            }
         }
     }
 
     handleFormInputChange = (event, key) => {
-        this.setState({
-            formData: {
-                ...this.state.formData,
-                [key]: event.target.value
-            }
-        })
+        const updatedFormData = updateObject(this.state.formData, {
+            [key]: updateObject(this.state.formData[key], {
+                value: event.target.value,
+                valid: checkValidity(event.target.value, this.state.formData[key].validation),
+                touched: true
+            })
+        });
+        this.setState({ formData: updatedFormData })
     }
 
     handleFormSubmit = () => {
-        const username = this.state.formData.username;
-        const password = this.state.formData.password;
+        const username = this.state.formData.username.value;
+        const password = this.state.formData.password.value;
 
         this.props.handleUserLogin({username, password})
     }
 
     render() {
+
+        let errorMessage = null;
+        if (this.props.error) {
+            debugger;
+            errorMessage = (
+                <p style={{color: "red", textTransform: 'capitalize'}}>{this.props.error.user_authentication}</p>
+            );
+        }
+
+        let submitDisabled = Object.values(this.state.formData).some(inputField => !inputField.valid )
+
+
         return (
             <Grid
                 textAlign='center'
@@ -48,6 +85,7 @@ class LoginForm extends Component {
                         </Header>
                     <Form size='large' onSubmit={() => this.handleFormSubmit()}>
                         <Segment stacked>
+                            {errorMessage}
                             <Form.Field
                                 fluid
                                 icon='user'
@@ -57,6 +95,7 @@ class LoginForm extends Component {
                                 id='username'
                                 value={this.state.username}
                                 onChange={event => this.handleFormInputChange(event, 'username')}
+                                className={this.state.formData.username.touched && !this.state.formData.username.valid ? classes.Invalid : null }
                             />
                             <Form.Field
                                 fluid
@@ -70,7 +109,7 @@ class LoginForm extends Component {
                                 onChange={event => this.handleFormInputChange(event, 'password')}
                             />
 
-                            <Button className={classes.LoginButton} fluid size='large'>Login</Button>
+                            <Button disabled={submitDisabled} className={classes.LoginButton} fluid size='large'>Login</Button>
                         </Segment>
                     </Form>
                 </Grid.Column>
@@ -79,9 +118,13 @@ class LoginForm extends Component {
     }
 
 }
+const mapStateToProps = (state) => ({
+    error: state.auth.error
+})
+
 
 const mapDispatchToProps = dispatch => (
     bindActionCreators({ handleUserLogin }, dispatch)
 )
 
-export default connect(null, mapDispatchToProps)(LoginForm)
+export default connect(mapStateToProps, mapDispatchToProps)(LoginForm)
