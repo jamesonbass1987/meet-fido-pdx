7794ab5911a5199026dc5282790542147d45b941
jamesonbass1987 <jamesonbass@gmail.com>
2018-02-26 11:06:22 -0800

    refactored user action and reducer functions

--- client/src/store/actions/user.js
+++ client/src/store/actions/user.js
@@ -1,136 +1,91 @@
 import * as actionTypes from './actionTypes';
 import axios from '../../shared/axios-api';
 
+// USER SHARED ACTIONS
+
+export const fetchUserAssetStart = () => ({
+    type: actionTypes.FETCH_USER_ASSET_START
+});
+
+export const fetchUserAssetFail = error => ({
+    type: actionTypes.FETCH_USER_ASSET_FAIL,
+    error
+});
 
 // USER INDEX ACTIONS
 
-export const fetchUsersSuccess = (users) => {
-    return {
-        type: actionTypes.FETCH_USERS_SUCCESS,
-        users: users
-    };
-};
-
-export const fetchUsersFail = (error) => {
-    return {
-        type: actionTypes.FETCH_USERS_FAIL,
-        error: error
-    };
-};
-
-export const fetchUsersStart = () => {
-    return {
-        type: actionTypes.FETCH_USERS_START
-    };
-};
-
 export const fetchUsers = () => {
     return dispatch => {
-
-        dispatch(fetchUsersStart());
+        dispatch(fetchUserAssetStart());
         axios.get('/users')
             .then(res => {
                 const fetchedUsers = [...res.data];
                 dispatch(fetchUsersSuccess(fetchedUsers));
             })
             .catch(err => {
-                dispatch(fetchUsersFail(err));
+                dispatch(fetchUserAssetFail(err));
             });
     };
 };
 
+export const fetchUsersSuccess = users => ({
+    type: actionTypes.FETCH_USERS_SUCCESS,
+    users
+});
+
 // USER SHOW ACTIONS
-
-export const fetchUserSuccess = (user) => {
-    return {
-        type: actionTypes.FETCH_USER_SUCCESS,
-        user
-    };
-};
-
-export const fetchUserFail = (error) => {
-    return {
-        type: actionTypes.FETCH_USER_FAIL,
-        error
-    };
-};
-
-export const fetchUserStart = () => {
-    return {
-        type: actionTypes.FETCH_USER_START
-    };
-};
 
 export const fetchUser = id => {
     return dispatch => {
-        dispatch(fetchUserStart());
+        dispatch(fetchUserAssetStart());
         axios.get(`/users/${id}`)
             .then(res => {
                 const fetchedUser = res.data;
                 dispatch(fetchUserSuccess(fetchedUser));
             })
             .catch(err => {
-                dispatch(fetchUserFail(err));
+                dispatch(fetchUserAssetFail(err));
             });
     };
 };
 
+export const fetchUserSuccess = user => ({
+    type: actionTypes.FETCH_USER_SUCCESS,
+    user
+});
 
 // USER DELETE ACTIONS
 
-export const deleteUserSuccess = () => {
-    return {
-        type: actionTypes.DELETE_USER_SUCCESS
-    };
-};
-
-export const deleteUserFail = (error) => {
-    return {
-        type: actionTypes.DELETE_USER_FAIL,
-        error: error
-    };
-};
-
-export const deleteUserStart = () => {
-    return {
-        type: actionTypes.DELETE_USER_START
-    };
-};
-
-export const deleteUser = (id) => {
+export const deleteUser = id => {
     return dispatch => {
-         dispatch(deleteUserStart())
-
+         dispatch(fetchUserAssetStart())
          axios.delete(`/users/${id}`)
             .then(resp => {
                 dispatch(deleteUserSuccess());
             })
             .catch(err => {
-                dispatch(deleteUserFail(err));
+                dispatch(fetchUserAssetFail(err));
             })
     }
 }
 
+export const deleteUserSuccess = () => ({
+    type: actionTypes.DELETE_USER_SUCCESS
+});
 
 
-// User filter functions
+// USER FILTER FUNCTIONS
 
-export const updateUserFilter = (type, value) => {
-    return {
-        type: actionTypes.UPDATE_USER_FILTER,
-        payload: { type, value }
-    };
-}
+export const updateUserFilter = (type, value) => ({
+    type: actionTypes.UPDATE_USER_FILTER,
+    payload: { type, value }
+});
 
-export const resetUserFilter = (type, value) => {
-    return {
-        type: actionTypes.RESET_USER_FILTER
-    };
-}
+export const resetUserFilter = (type, value) => ({
+    type: actionTypes.RESET_USER_FILTER
+});
 
-export const removeSelectedUser = () => {
-    return {
-        type: actionTypes.REMOVE_SELECTED_USER
-    }
-}
+export const removeSelectedUser = () => ({
+    type: actionTypes.REMOVE_SELECTED_USER
+});
 
--- client/src/store/reducers/user.js
+++ client/src/store/reducers/user.js
@@ -12,89 +12,63 @@ const initialState = {
     isUpdating: false
 }
 
-const fetchUsersStart = state => {
-    return updateObject(state, { loading: true });
-};
+const fetchUserAssetStart = state => (updateObject(state, { loading: true }));
+const fetchUserAssetFail = (state, action) => (updateObject(state, { loading: false, error: action.error }));
 
-const fetchUsersSuccess = (state, action) => {
-    return updateObject(state, {
-        users: action.users,
-        loading: false,
-    });
-};
-
-const fetchUsersFail = state => {
-    return updateObject(state, { loading: false });
-};
-
-const fetchUserStart = state => {
-    return updateObject(state, { loading: true });
-};
-
-const fetchUserSuccess = (state, action) => {
-    return updateObject(state, {
+const fetchUserSuccess = (state, action) => (
+    updateObject(state, {
         selectedUser: action.user,
         loading: false
-    });
-};
+    })
+);
 
-const fetchUserFail = state => {
-    return updateObject(state, { loading: false });
-};
-
-const deleteUserStart = state => {
-    return updateObject(state, { loading: true });
-};
-
-const deleteUserSuccess = (state, action) => {
-    return updateObject(state, {
-        error: null,
+const fetchUsersSuccess = (state, action) => (
+    updateObject(state, {
+        users: action.users,
         loading: false,
-    });
-};
+    })
+);
 
-const deleteUserFail = state => {
-    return updateObject(state, { loading: false });
-};
+const deleteUserSuccess = (state, action) => (
+    updateObject(state, {
+        loading: false,
+    })
+);
 
-const updateUserFilter = (state, action) => {
-    return updateObject(state, {
+const removeSelectedUser = state => (
+    updateObject(state, {
+        selectedUser: null
+    })
+);
+
+const updateUserFilter = (state, action) => (
+    updateObject(state, {
         userFilter: {
             ...state.userFilter,
             [action.payload.type]: action.payload.value
         }
     })
-}
+);
 
-const resetUserFilter = (state, action) => {
-    return updateObject(state, {
+const resetUserFilter = (state, action) => (
+    updateObject(state, {
         userFilter: {
             searchQuery: '',
             selectedPark: ''
         }
     })
-}
-
-const removeSelectedUser = (state) => {
-    return updateObject(state, {
-        selectedUser: null
-    })
-}
+);
 
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
-        case actionTypes.FETCH_USERS_START: return fetchUsersStart(state);
-        case actionTypes.FETCH_USERS_SUCCESS: return fetchUsersSuccess(state, action);
-        case actionTypes.FETCH_USERS_FAIL: return fetchUsersFail(state);
-        case actionTypes.FETCH_USER_START: return fetchUserStart(state);
+        case actionTypes.FETCH_USER_ASSET_START: return fetchUserAssetStart(state);
+        case actionTypes.FETCH_USER_ASSET_FAIL: return fetchUserAssetFail(state, action);
         case actionTypes.FETCH_USER_SUCCESS: return fetchUserSuccess(state, action);
-        case actionTypes.FETCH_USER_FAIL: return fetchUserFail(state);
+        case actionTypes.FETCH_USERS_SUCCESS: return fetchUsersSuccess(state, action);
+        case actionTypes.DELETE_USER_SUCCESS: return deleteUserSuccess(state);
+        case actionTypes.REMOVE_SELECTED_USER: return removeSelectedUser(state);
         case actionTypes.UPDATE_USER_FILTER: return updateUserFilter(state, action);
         case actionTypes.RESET_USER_FILTER: return resetUserFilter(state, action);
-        case actionTypes.DELETE_USER_START: return deleteUserStart(state);
-        case actionTypes.DELETE_USER_SUCCESS: return deleteUserSuccess(state);
-        case actionTypes.DELETE_USER_FAIL: return deleteUserFail(state);
-        case actionTypes.REMOVE_SELECTED_USER: return removeSelectedUser(state);
         default: return state
     }
 }
