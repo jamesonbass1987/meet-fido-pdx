ee482df3b2dc9e245b0f92547205b82e22e9fc4a
jamesonbass1987 <jamesonbass@gmail.com>
2018-03-07 08:29:16 -0800

    udpated navbar update logic

--- client/src/containers/Navbar/Navbar.js
+++ client/src/containers/Navbar/Navbar.js
@@ -9,6 +9,10 @@ import NavigationItems from '../../components/NavigationItems/NavigationItems'
 
 class Navbar extends Component {
 
+    shouldComponentUpdate = (nextProps) => (
+        this.props.currentUser !== nextProps.currentUser
+    );
+
     render() {
         let navLinks = [];
         
