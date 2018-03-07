f0c91619430ee3a4e7641d19d3e892f5c4cd8c18
jamesonbass1987 <jamesonbass@gmail.com>
2018-03-07 07:27:39 -0800

    temp removed auth on update/create actions

--- app/controllers/api/v1/dogs_controller.rb
+++ app/controllers/api/v1/dogs_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::DogsController < ApiController
-  before_action :authenticate_request, only: [:create, :update, :destroy]
+  # before_action :authenticate_request, only: [:create, :update, :destroy]
   before_action :set_dog, only: [:show, :update, :destroy]
 
   def index
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::UsersController < ApiController
-  before_action :authenticate_request, only: [:update, :destroy]
+  # before_action :authenticate_request, only: [:update, :destroy]
   before_action :set_user, only: [:show, :update, :destroy]
 
   def index
